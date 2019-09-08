5c5f45d35ce21a17420a7c3c6d373fa4f4061495
Befezdow <befezd@gmail.com>
2019-09-09 00:00:18 +0300

    added login form

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
-  <v-app app>
-    <nuxt/>
+  <v-app app dark>
+    <v-content>
+      <v-container fluid>
+        <nuxt/>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>
-    <v-btn @click="onSendMessageClick">Send "GL HF"</v-btn>
-  </div>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8>
+      <v-card min-width="400">
+        <v-card-title>
+          ReChat
+        </v-card-title>
+        <v-card-text>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+              v-model="name"
+              :rules="nameRules"
+              :counter="16"
+              label="Ваше имя"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              :counter="16"
+              label="Название комнаты"
+              required
+            ></v-text-field>
+
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              class="mr-4"
+              @click="submit"
+            >
+              Войти
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -11,12 +49,26 @@
         console.log('Client IO connected');
       }
     },
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Введите имя',
+        v => (v && v.length <= 16) || 'Имя не должно превышать 16 символов'
+      ],
+      room: '',
+      roomRules: [
+        v => !!v || 'Введите название комнаты',
+        v => (v && v.length <= 16) || 'Название комнаты не должно превышать 16 символов'
+      ]
+    }),
+
     methods: {
-      onSendMessageClick() {
-        const messageText = 'GL HF';
-        this.$socket.emit('request', { text: messageText });
-        console.log(`Client: ${messageText}`);
+      submit () {
+        if (this.$refs.form.validate()) {
+
+        }
       }
     }
   }
-</script>>
+</script>
